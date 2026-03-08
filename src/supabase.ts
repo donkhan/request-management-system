56c6fc8f471460e72f5dac67136fa7aaace127f4
Kamil Khan <kamilkhan@rediffmail.com.>
2026-03-08 17:55:41 +0530

    put the info in env variables

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,8 +1,6 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
-const supabaseUrl =
-  "https://pvkrijsprcwvguhocwke.supabase.co";
-
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2a3JpanNwcmN3dmd1aG9jd2tlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2NzEwOTQsImV4cCI6MjA4NzI0NzA5NH0.kN5mQ9XGWv7CUE1q4h6ijPfrVAWEX_bGfSMtHcmFskw'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 let supabaseInstance: SupabaseClient | null = null;
 
