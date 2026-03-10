6ae7cc1649dc8b9628bc9241874936a5352dd70d
Kamil Khan <kamilkhan@rediffmail.com.>
2026-03-10 19:42:33 +0530

    show google accounts

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -5,11 +5,20 @@ export async function loginWithGoogle() {
     provider: "google",
     options: {
       redirectTo: window.location.origin,
+      queryParams: {
+        prompt: "select_account"
+      }
     },
   });
 }
 
 export async function logout() {
   await getSupabase().auth.signOut();
+  localStorage.removeItem("supabase.auth.token");
+  Object.keys(localStorage).forEach((key) => {
+  if (key.startsWith("sb-")) {
+    localStorage.removeItem(key);
+  }
+});
   window.location.reload();
 }
